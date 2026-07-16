f22dd380ee249c6d4944b7f9fdc4434a0a10b805
Nechitaylo Nikita <nikitanechitailo93@gmail.com>
2026-07-16 18:34:52 +0200

    change step sound and add shot sound

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,33 +2,58 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource sound;
+    [SerializeField] private AudioSource footstep_sound;
     [SerializeField] private AudioSource sound1;
 
 
     private void OnEnable()
     {
         MyInputManager.OnSpacePressed += PlayAudio;
+        MyInputManager.OnMovePressed += CheckForMovement;
     }
 
     private void OnDisable()
     {
         MyInputManager.OnSpacePressed -= PlayAudio;
+        MyInputManager.OnMovePressed -= CheckForMovement;
     }
     private void PlayAudio()
     {
-        if (!sound.isPlaying)
+        if (!sound1.isPlaying)
         {
-            sound.Play();
+            sound1.Play();
             return;
         }
         // sound.Stop();
         // sound.PlayOneShot(sound.clip);
-        if (sound.isPlaying)
+        if (sound1.isPlaying)
         {
-            sound.Stop();
+            sound1.Stop();
             return;
         }
 
+    }
+
+    private void PlayFootStepAudio(bool isPlaying)
+    {
+        if (!footstep_sound.isPlaying && isPlaying)
+        {
+            footstep_sound.Play();
+            return;
+        }
+        // sound.Stop();
+        // sound.PlayOneShot(sound.clip);
+        if (footstep_sound.isPlaying && !isPlaying)
+        {
+            footstep_sound.Stop();
+            return;
+        }
+
+    }
+
+    private void CheckForMovement(Vector2 input)
+    {
+        bool toPlay = input.sqrMagnitude >= 0.2f;
+        PlayFootStepAudio(toPlay);
     }
 }
